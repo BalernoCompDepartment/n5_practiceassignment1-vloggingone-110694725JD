981e99768237eabd7f793f47a628dc50ff6e4757
110694725JD <110694725@ea.edin.sch.uk>
2021-03-19 09:52:41 +0000

    added the validations

--- Main.sql
+++ Main.sql
@@ -10,7 +10,11 @@ vloggerID int NOT NULL,
 forename varchar(255) NOT NULL,
 surname varchar(255) NOT NULL,
 username varchar(255) NOT NULL,
-expertise varchar(255) NOT NULL
+expertise varchar(255) NOT NULL,
+PRIMARY KEY (vloggerID),
+CHECK(length (username) = 6),
+CHECK (expertise IN("programming", "gaming", "baking", "crafts", "makeup", "clothes"))
+
 
 
 /* Adding validation to the table from the datadictionary*/
